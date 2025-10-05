8976da898cddbbe4e623b4eee84682350861ffa9
Jackson Clark <124289919+jaxwc@users.noreply.github.com>
2025-10-05 15:44:02 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,42 @@
+brew install 1password-cli
+brew install 1password@beta
+brew install aerospace
+brew install alt-tab
+brew install cleanshot
+brew install fantastical
+brew install ghostty
+brew install gimp
+brew install legcord
+# brew install linearmouse
+brew install obsidian
+brew install raycast
+brew install spotify
+brew install visual-studio-code
+brew install zen
+
+brew tap FelixKratz/formulae
+brew install sketchybar
+brew install borders
+
+brew install --cask nikitabobko/tap/aerospace
+
+brew tap epk/epk
+brew install font-sf-mono-nerd-font
+brew install font-sf-pro
+brew install font-sketchybar-app-font
+brew install sf-symbols
+
+brew install nvim
+brew install tmux
+brew install starship
+
+brew install zsh-autosuggestions
+
+brew install python
+brew install node
+brew install imagemagick
+brew install lua
+brew install ripgrep
+brew install git
+brew install ifstat
+brew install switchaudio-osx
